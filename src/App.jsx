9096f8fdfe7a8d25706c8b7180fb24a3445a264c
atharvguptaaa <workinatharv@gmail.com>
2024-08-17 11:49:46 +0530

    width resolve

--- src/App.jsx
+++ src/App.jsx
@@ -42,11 +42,11 @@ function App() {
     return (
         <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
             <div className="bg-[#4c4c97] min-h-screen py-8 flex justify-center items-center font-mono">
-                <div className="w-full max-w-lg mx-auto shadow-lg rounded-lg px-4 py-6 bg-white">
+                <div className="md:w-full md:max-w-lg mx-auto shadow-lg rounded-lg px-4 py-6 bg-white">
                     <h1 className="text-3xl font-mono font-semibold text-center mb-8 text-[#1e132d]">
                         Manage Your Todos
                     </h1>
-                    <div className="py-4 sm:px-8 md:px-16 mb-4 sm:my-3">
+                    <div className="w-full mb-6">
                         <TodoForm />
                     </div>
                     <div className="flex flex-col gap-4">
--- src/component/TodoForm.jsx
+++ src/component/TodoForm.jsx
@@ -15,19 +15,19 @@ export function TodoForm() {
     };
 
     return (
-        <div className="px-4 sm:px-8 md:px-16">
-            <form onSubmit={add} className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-0">
-                <input
-                    type="text"
-                    placeholder="Write Todo..."
-                    className="w-full border border-[#1e132d] rounded-lg sm:rounded-l-lg sm:rounded-r-none px-3 outline-none duration-150 bg-white py-2 text-[#4D4D4D] placeholder-[#BF5F82] focus:ring-[#BF5F82] focus:border-[#BF5F82]"
-                    value={todoMsg}
-                    onChange={(e) => setTodoMsg(e.target.value)}
-                />
-                <button type="submit" className="w-full sm:w-auto rounded-lg sm:rounded-r-lg sm:rounded-l-none px-4 py-2 bg-[#1e132d] text-white border border-[#1e132d] hover:bg-[#4c4c97] hover:border-[#4c4c97]">
-                    Add
-                </button>
-            </form>
-        </div>
+        <form onSubmit={add} className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-0">
+            <input
+                type="text"
+                placeholder="Write Todo..."
+                className="w-full border border-[#1e132d] rounded-lg sm:rounded-l-lg sm:rounded-r-none px-3 outline-none duration-150 bg-white py-2 text-[#4D4D4D] placeholder-[#BF5F82] focus:ring-[#BF5F82] focus:border-[#BF5F82]"
+                value={todoMsg}
+                onChange={(e) => setTodoMsg(e.target.value)}
+            />
+            <button type="submit" className="w-full sm:w-auto rounded-lg sm:rounded-r-lg sm:rounded-l-none px-4 py-2 bg-[#1e132d] text-white border border-[#1e132d] hover:bg-[#4c4c97] hover:border-[#4c4c97]">
+                Add
+            </button>
+        </form>
     );
 }
+
+
